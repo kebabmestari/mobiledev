d106ce56aa69110714891d76ecfdf992a0765fca
Admin <Admin>
2016-04-28 00:20:26 +0300

    Kameratominto lisätty

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,22 +1,81 @@
 angular.module('mobileApp.controllers', [])
 
-.controller('ListCtrl', function($scope) {})
+.controller('MainCtrl', function($scope, $cordovaCamera, $cordovaFile) {
+    $scope.images = [];
 
-.controller('LocalStorageCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+    $scope.addImage = function() {
+        var options = {
+            destinationType : Camera.DestinationType.FILE_URI,
+            sourceType : Camera.PictureSourceType.CAMERA, // Camera.PictureSourceType.PHOTOLIBRARY
+            allowEdit : false,
+            encodingType: Camera.EncodingType.JPEG,
+            popoverOptions: CameraPopoverOptions,
+        };
+
+        $cordovaCamera.getPicture(options).then(function(imageData) {
+            onImageSuccess(imageData);
+
+            function onImageSuccess(fileURI) {
+                createFileEntry(fileURI);
+            }
+
+            function createFileEntry(fileURI) {
+                window.resolveLocalFileSystemURL(fileURI, copyFile, fail);
+            }
+
+            function copyFile(fileEntry) {
+                var name = fileEntry.fullPath.substr(fileEntry.fullPath.lastIndexOf('/') + 1);
+                var newName = makeid() + name;
+
+                window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function(fileSystem2) {
+                    fileEntry.copyTo(
+                        fileSystem2,
+                        newName,
+                        onCopySuccess,
+                        fail
+                    );
+                },
+                fail);
+            }
+
+            function onCopySuccess(entry) {
+                $scope.$apply(function () {
+                    $scope.images.push(entry.nativeURL);
+                });
+            }
+
+            function fail(error) {
+                console.log("fail: " + error.code);
+            }
+
+            function makeid() {
+                var text = "";
+                var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+                for (var i=0; i < 5; i++) {
+                    text += possible.charAt(Math.floor(Math.random() * possible.length));
+                }
+                return text;
+            }
+
+        }, function(err) {
+            console.log(err);
+        });
+    }
+
+    $scope.urlForImage = function(imageName) {
+        var name = imageName.substr(imageName.lastIndexOf('/') + 1);
+        var trueOrigin = cordova.file.dataDirectory + name;
+        return trueOrigin;
+    }
+
 })
 
-.controller('CloudStorageCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+.controller('LocalStorageCtrl', function($scope ) {
+
+})
+
+.controller('CloudStorageCtrl', function($scope, $stateParams) {
+
 });
